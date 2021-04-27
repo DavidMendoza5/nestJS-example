2521dd26ca713bb3e128e4a8ff123f683b49aed5
David <cruz-azul99@hotmail.com>
2021-04-27 16:03:56 -0500

    Code commented on create method

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+// import { plainToClass } from 'class-transformer';
+// import { User } from 'src/User/model/user.model';
+// import { UserService } from 'src/User/user.service';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateCourseDto } from './dto/create.course.dto';
 import { Course } from './model/course.model';
@@ -8,6 +11,10 @@ const courses: Course[] = [];
 @Injectable()
 export class CourseService {
   public create(courseDto: CreateCourseDto, userId:string): void {
+    // let userService: object;
+    // const realUserService = plainToClass(UserService, userService)
+    // const userArray: User[] = realUserService.findAll();
+    // console.log(userArray);
     courses.push(new Course(uuidv4(), courseDto.coursename, userId));
   }
 
