515250bfb0f9cf2b924b9764427c3e685ce466ec
David <cruz-azul99@hotmail.com>
2021-05-03 21:16:26 -0500

    UserService imported and added to providers

--- src/course/course.module.ts
+++ src/course/course.module.ts
@@ -1,10 +1,11 @@
 import { CourseController } from './course.controller';
 import { Module } from '@nestjs/common';
 import { CourseService } from './course.service';
+import { UserService } from 'src/User/user.service';
 
 @Module({
-  imports: [],
+  imports: [UserService],
   controllers: [CourseController],
-  providers: [CourseService],
+  providers: [CourseService, UserService],
 })
 export class CourseModule {}
