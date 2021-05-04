6041ca5a0c5d6a5196f42afc8f834980f80916b9
David <cruz-azul99@hotmail.com>
2021-05-03 21:15:32 -0500

    UserService added - UserService provides us the user's array

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/User/model/user.model';
+import { UserService } from 'src/User/user.service';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateCourseDto } from './dto/create.course.dto';
 import { Course } from './model/course.model';
@@ -7,7 +9,10 @@ const courses: Course[] = [];
 
 @Injectable()
 export class CourseService {
+  constructor(private userService: UserService) {}
   public create(courseDto: CreateCourseDto, userId:string): void {
+    const userArray: User[] = this.userService.findAll();
+    console.log(userArray);
     courses.push(new Course(uuidv4(), courseDto.coursename, userId));
   }
 
