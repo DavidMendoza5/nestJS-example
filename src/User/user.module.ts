2d271cfe5aa02c1a802b6dc574900eee80878151
David <cruz-azul99@hotmail.com>
2021-05-29 14:30:18 -0500

    TypeOrm module and unit of work imported

--- src/User/user.module.ts
+++ src/User/user.module.ts
@@ -1,9 +1,13 @@
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserSchema } from './model/schema/user.schema';
+import { UnitOfWork } from 'src/share/unitofwork/unit-of-work.provider';
+// import {  } from './repository/user.repository';
 
 @Module({
-    imports: [],
+    imports: [TypeOrmModule.forFeature([UserSchema]), UnitOfWork],
     controllers: [
         UserController, ],
     providers: [
