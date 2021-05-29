966ec90debd03a7539811e7a6118aee127469e4e
David <cruz-azul99@hotmail.com>
2021-05-29 14:30:48 -0500

    Create method added

--- src/User/user.service.ts
+++ src/User/user.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Connection, EntityManager, getManager } from 'typeorm';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateUserDto } from './dto/create.user.dto';
 import { User } from './model/user.model'
@@ -7,8 +8,13 @@ const users: User[] = [];
 
 @Injectable()
 export class UserService {
-  public create(userDto: CreateUserDto): void {
-    users.push(new User(uuidv4(), userDto.name, userDto.username));
+  constructor(private connection: Connection) {}
+
+  public async create(userDto: CreateUserDto): Promise<void> {
+    const manager: EntityManager = this.connection.manager;
+    await manager.save(User, new User(userDto.name, userDto.username));
+    // const queryRunner = this.connection.createQueryRunner()
+    // users.push(new User(uuidv4(), userDto.name, userDto.username));
   }
 
   public findAll(): User[] {
